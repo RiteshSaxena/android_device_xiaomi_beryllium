949b0327fce9a61b7fb3e8412ad67bf1ab5a422f
Ritesh Saxena <riteshsax007@gmail.com>
2018-10-21 07:55:39 +0000

    beryllium: Reloaded bringup
    Change-Id: I6a0ea7573fa6123b667bd4f6c41494ce3d74ad07

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beryllium.mk
+    $(LOCAL_DIR)/reloaded_beryllium.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beryllium-user \
-    lineage_beryllium-userdebug \
-    lineage_beryllium-eng
+    reloaded_beryllium-user \
+    reloaded_beryllium-userdebug \
+    reloaded_beryllium-eng

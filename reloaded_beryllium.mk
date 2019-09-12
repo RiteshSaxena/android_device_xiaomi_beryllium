57a34409f91cda23a116bd303d735154daab64e2
LuK1337 <priv.luk@gmail.com>
2019-09-12 23:57:51 +0200

    beryllium: Stop overriding TARGET_DEVICE
    Not only TARGET_DEVICE is no longer used for generating props
    (as tools/buildinfo_common.sh is using PRODUCT_DEVICE instead),
    but also there's no need to override PRODUCT_DEVICE because
    it is already set to the desired value.
    
    Change-Id: I16bd7b5bfaab5225478921f2b532c40a99eda972

--- reloaded_beryllium.mk
+++ reloaded_beryllium.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2018-2019 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -20,7 +20,6 @@ BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:10/QKQ1.190828.002/V11.0.6.0.QE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.6.0.QEJMIXM release-keys" \
-    PRODUCT_NAME="beryllium" \
-    TARGET_DEVICE="beryllium"
+    PRODUCT_NAME="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1

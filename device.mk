34233042e7288c327e72ed70f12f52856429e98a
Ritesh Saxena <riteshsax007@gmail.com>
2019-10-12 13:24:14 +0000

    beryllium: Drop PocketMode
    Change-Id: Ic67319fdd6f5a59ef30583bd78e2f695ac456db2

--- device.mk
+++ device.mk
@@ -64,10 +64,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:system/usr/keylayout/uinput-goodix.kl
 
-# PocketMode
-PRODUCT_PACKAGES += \
-    XiaomiPocketMode
-
 # ThermalController
 PRODUCT_PACKAGES += \
     ThermalController
